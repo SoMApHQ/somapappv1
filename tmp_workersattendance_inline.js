26aada1fd4ce0b9ce85b71723b00e43b78d7a92c
Mnyore Joseph <mnyorej@gmail.com>
2026-03-28 16:35:49 +0300

    check in

---
+++ tmp_workersattendance_inline.js
@@ -0,0 +1,5 @@
+﻿
+    // Required by finance_math.js for anchor enrollment year
+    var SOMAP_DEFAULT_YEAR = new Date().getFullYear();
+    var SOMAP_ALLOWED_YEARS = Array.from({length: 10}, (_, i) => SOMAP_DEFAULT_YEAR - 2 + i);
+  
